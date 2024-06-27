23d29619039161e7949a53ee16dd58ab9b2d4c56
Chandrika <133841590+Chandrika11111@users.noreply.github.com>
2024-06-27 11:17:34 +0530

    Update apiPath.js

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,2 +1,2 @@
 // export const API_URL='http://localhost:4000';
- export const API_URL='https://food-restaurant-project-backend-1.onrender.com/'
+ export const API_URL='https://food-restaurant-project-backend-1.onrender.com'
